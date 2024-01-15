0aa03b25b9d55143f2ca38968364410760b45efd
xfactor-toml <xfactor.toml@gmail.com>
2024-01-16 00:35:47 +0800

    fix: update api looped call

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,24 @@
 const controller = require('./controller/controller');
 
+let result;
+const getData = (data) => {
+    result = data;
+}
+
+controller.getTickerPriceForSocket(getData);
+setInterval(() => {
+    controller.getTickerPriceForSocket(getData);
+}, 15000);
+
+
 exports = module.exports = server = (io) => {
     io.on('connection', (socket) => {
         console.log(`--- A socket ${socket.id} connected! ---`);
-        
-        const getData = (data) => {
-            socket.emit('send_token', data);
-        }
-        controller.getTickerPriceForSocket(getData);
+        socket.emit('send_token', result);
         setInterval(() => {
-            controller.getTickerPriceForSocket(getData);
-        }, 30000);
+            socket.emit('send_token', result);
+        }, 5000);
+
         socket.on('send_data', (data) => {
             socket.emit('receive_data', data);
         });
