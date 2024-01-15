399334293fbb41e0fce2badb16d88c8f0519216d
xfactor-toml <xfactor.toml@gmail.com>
2024-01-15 17:43:32 +0800

    chore: change cors on backend

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const socket = require('./server');
 const server = http.createServer(app);
 
 const io = require('socket.io')(server, {
-    cors: {origin: 'http://localhost:5173', methods: ['GET', 'POST']},
+    cors: {origin: '*', methods: ['GET', 'POST']},
 });
 
 socket(io);
