c69fd55df5f757fc65f64b63f6fd5fcedf910f19
JP <jp@email.com>
2024-01-16 17:35:52 +0900

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,24 @@
 const controller = require('./controller/controller');
 
+let result;
+const getData = (data) => {
+    result = data;
+    console.log(result);
+}
+
+controller.getTickerPriceForSocket(getData);
+setInterval(() => {
+    controller.getTickerPriceForSocket(getData);
+}, 30000);
+
 exports = module.exports = server = (io) => {
     io.on('connection', (socket) => {
         console.log(`--- A socket ${socket.id} connected! ---`);
         
-        const getData = (data) => {
-            socket.emit('send_token', data);
-        }
-        controller.getTickerPriceForSocket(getData);
+        socket.emit('send_token', result);
         setInterval(() => {
-            controller.getTickerPriceForSocket(getData);
-        }, 30000);
-        socket.on('send_data', (data) => {
-            socket.emit('receive_data', data);
-        });
+            socket.emit('send_token', result);
+        }, 5000);
         socket.on('disconnect', () => {
             console.log('disconnected');
         });
