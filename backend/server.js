cc12ef81717c0dbad8b6244723640883205a2015
JP <jp@email.com>
2024-01-19 14:23:21 +0900

    fea: websocket construct

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,38 @@
 const controller = require('./controller/controller');
+const websocekt = require('./ws');
 
-let result;
+let token3min;
+let realTimeTokens;
 const getData = (data) => {
     console.log(data);
-    result = data;
+    token3min = data;
 }
+
+const getRealData = (data) => {
+    console.log(data?.realTimeData.length);
+    realTimeTokens = data;
+}
+
+websocekt.getRealTimeData(getRealData);
 
 controller.getTickerPriceForSocket(getData);
 setInterval(() => {
     controller.getTickerPriceForSocket(getData);
-}, 30000);
+}, 15000);
 
 
 exports = module.exports = server = (io) => {
     io.on('connection', (socket) => {
         console.log(`--- A socket ${socket.id} connected! ---`);
         
-        socket.emit('send_token', result);
+        socket.emit('token3min', token3min);
         setInterval(() => {
-            socket.emit('send_token', result);
+            socket.emit('token3min', token3min);
         }, 5000);
+
+        setInterval(() => {
+            socket.emit('realTimeData', realTimeTokens);
+        }, 1000);
 
         socket.on('send_data', (data) => {
             socket.emit('receive_data', data);
