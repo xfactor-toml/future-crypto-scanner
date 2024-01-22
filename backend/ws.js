791a784a90ba4b9d7bae992306d7bfb89cd140bc
JP <jp@email.com>
2024-01-22 11:00:08 +0900

    fix: webscoket reconnect

--- backend/ws.js
+++ backend/ws.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 exports.getRealTimeData =async (getData) => {
 
-  //----------first request for total ticker that is between 0.01 and 2 and more than 1 million. this is call once-------------------//
+  //----------first request for total ticker that is between 0.01 and 2 and more than 1 million. this is called once-------------------//
   var realTimeData = [];
   const tickerPriceList = await axios.get('https://fapi.binance.com/fapi/v2/ticker/price')
   const tokensBetween = tickerPriceList.data.filter(item => Number(item.price) >= 0.01 && Number(item.price) <= 2);// tokens between 0.01~2
@@ -11,18 +11,13 @@ exports.getRealTimeData =async (getData) => {
           'Content-Type': 'application/json'
         });
     if (Number(_1dKline.data[0][7]) >= 1000000)
-    // {
-    //   realTimeData[item.symbol.toLowerCase().slice(0, -4)] = {
-    //     'price' : Number(item?.price).toFixed(4),
-    //     'volume': Number(_1dKline?.data[0][7]).toFixed(4)
-    //   }
-    // }
     realTimeData.push({
       'symbol': item.symbol.toLowerCase().slice(0, -4),
       'price' : Number(item?.price).toFixed(4),
       'volume': Number(_1dKline?.data[0][7]).toFixed(4),
     });
   })).then(() => {
+    console.log(realTimeData.length);
     getData({
             'realTimeData' : realTimeData,
             'status' : 'ok',
@@ -35,54 +30,65 @@ exports.getRealTimeData =async (getData) => {
   })
   //--------------receive all changed ticker per 1 second and replace it to origin realTimeData.
   const WebSocket = require('ws');
-  const socket = new WebSocket('wss://fstream.binance.com/stream?streams=!ticker@arr'); // all changed tickers
-  socket.onmessage = (event) => {
-    const data = JSON.parse(event.data);
-    const miniTickers = data.data;
-    // console.log('count', miniTickers.length);
-
-    // realTimeData = realTimeData.filter((i) => {
-
-    // })
-
-    Promise.all(miniTickers.map(item => {
-      if(Number(item.c) > 0.01 && Number(item.c) < 2 && Number(item.q) > 1000000) {      //  if 0.01 ~ 2
-      if(realTimeData.find((t) => t.symbol === item.s.toLowerCase().slice(0, -4)))   // if already exists, replace it.
-          realTimeData.splice(
-          realTimeData.findIndex((t) => t.symbol === item.s.toLowerCase().slice(0, -4)) 
-          , 1, 
-          {
-            'symbol': item.s.toLowerCase().slice(0, -4),
-            'price' : Number(item.c).toFixed(4),
-            'volume': Number(item.q).toFixed(4),
-          });
-      else                                                                              // if not exists, push.            
-          realTimeData.push(
+  const websocketThread = () => {
+    var socket = new WebSocket('wss://fstream.binance.com/stream?streams=!ticker@arr'); // all changed tickers
+    socket.onopen = () => {
+  
+    }
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      const miniTickers = data.data;
+  
+      Promise.all(miniTickers.map(item => {
+        if(Number(item.c) > 0.01 && Number(item.c) < 2 && Number(item.q) > 1000000) {      //  if 0.01 ~ 2
+        if(realTimeData.find((t) => t.symbol === item.s.toLowerCase().slice(0, -4)))   // if already exists, replace it.
+            realTimeData.splice(
+            realTimeData.findIndex((t) => t.symbol === item.s.toLowerCase().slice(0, -4)) 
+            , 1, 
             {
               'symbol': item.s.toLowerCase().slice(0, -4),
               'price' : Number(item.c).toFixed(4),
               'volume': Number(item.q).toFixed(4),
-            }
-          )}
-      else {
-        if(realTimeData.find((t) => t.symbol === item.s.toLowerCase().slice(0, -4)))
-        realTimeData.splice(
-          realTimeData.findIndex((t) => t.symbol === item.s.toLowerCase().slice(0, -4)) , 1);
-      }                                                                              // if < 0.01 or > 2, delete item.
-    }))
-    .then(() => {
-        getData({
-          'realTimeData' : realTimeData,
-          'status' : 'ok',
-        });
-    })
-    .catch(error => {
-        getData({
-          'realTimeData' : [],
-          'status' : error,
-        });
-    });
+            });
+        else                                                                              // if not exists, push.            
+            realTimeData.push(
+              {
+                'symbol': item.s.toLowerCase().slice(0, -4),
+                'price' : Number(item.c).toFixed(4),
+                'volume': Number(item.q).toFixed(4),
+              }
+            )}
+        else {
+          if(realTimeData.find((t) => t.symbol === item.s.toLowerCase().slice(0, -4)))
+          realTimeData.splice(
+            realTimeData.findIndex((t) => t.symbol === item.s.toLowerCase().slice(0, -4)) , 1);
+        }                                                                              // if < 0.01 or > 2, delete item.
+      }))
+      .then(() => {
+          getData({
+            'realTimeData' : realTimeData,
+            'status' : 'ok',
+          });
+      })
+      .catch(error => {
+          getData({
+            'realTimeData' : [],
+            'status' : error,
+          });
+      });
+    }
+    socket.onerror = error => {
+      console.log('binance websocket error occured, so reconnected');
+      socket = null;
+      setTimeout(websocketThread, 1000);
+    }
+    socket.onclose = () => {
+      console.log('binance websocket socket closed, so reconnected');
+      socket = null;
+      setTimeout(websocketThread, 1000);
+    }
   }
+  websocketThread();
 }
 
 //const klineStream = `${symbol.toLowerCase()}@kline_3m`;
