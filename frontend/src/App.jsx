f7eddc6491ffd26db090210053d531d1b08ada18
yamamotoyuki120 <yamamotoyuki120@gmail.com>
2024-02-02 13:20:55 +0900

    add hsitory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     document.querySelector('html').style.scrollBehavior = 'auto'
     window.scroll({ top: 0 })
     document.querySelector('html').style.scrollBehavior = ''
-  }, [location.pathname]); // triggered on route change
+  }, [location.pathname]); 
 
   return (
     <>
