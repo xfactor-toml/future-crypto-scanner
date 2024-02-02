56c689ae5ee9a631cff3cde40d3e35ea8db7f7a5
yamamotoyuki120 <yamamotoyuki120@gmail.com>
2024-02-02 13:40:20 +0900

    add history

--- frontend/src/partials/Sidebar.jsx
+++ frontend/src/partials/Sidebar.jsx
@@ -221,6 +221,52 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                   );
                 }}
               </SidebarLinkGroup>
+              <SidebarLinkGroup activecondition={pathname.includes('')}>
+                {(handleClick, open) => {
+                  return (
+                    <React.Fragment>
+                      {/* <a
+                        href="#0"
+                        className={`block text-slate-200 truncate transition duration-150 ${
+                          pathname.includes('') ? 'hover:text-slate-200' : 'hover:text-white'
+                        }`}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          sidebarExpanded ? handleClick() : setSidebarExpanded(true);
+                        }}
+                      > */}
+                        <div className="flex items-center justify-between">
+                        <Link to="/short">
+                          <div className="flex items-center">
+                            <svg className="shrink-0 h-10 w-10" viewBox="0 0 24 24">
+                              <path
+                                className={`fill-current ${pathname.includes('') ? 'text-indigo-500' : 'text-slate-600'}`}
+                                d="M19.714 14.7l-7.007 7.007-1.414-1.414 7.007-7.007c-.195-.4-.298-.84-.3-1.286a3 3 0 113 3 2.969 2.969 0 01-1.286-.3z"
+                              />
+                              <path
+                                className={`fill-current ${pathname.includes('') ? 'text-indigo-300' : 'text-slate-400'}`}
+                                d="M10.714 18.3c.4-.195.84-.298 1.286-.3a3 3 0 11-3 3c.002-.446.105-.885.3-1.286l-6.007-6.007 1.414-1.414 6.007 6.007z"
+                              />
+                              <path
+                                className={`fill-current ${pathname.includes('') ? 'text-indigo-500' : 'text-slate-600'}`}
+                                d="M5.7 10.714c.195.4.298.84.3 1.286a3 3 0 11-3-3c.446.002.885.105 1.286.3l7.007-7.007 1.414 1.414L5.7 10.714z"
+                              />
+                              <path
+                                className={`fill-current ${pathname.includes('') ? 'text-indigo-300' : 'text-slate-400'}`}
+                                d="M19.707 9.292a3.012 3.012 0 00-1.415 1.415L13.286 5.7c-.4.195-.84.298-1.286.3a3 3 0 113-3 2.969 2.969 0 01-.3 1.286l5.007 5.006z"
+                              />
+                            </svg>
+                            <span className="text-lg font-medium ml-3 duration-200">
+                              Short Signal
+                            </span>
+                          </div>
+                          </Link>
+                        </div>
+                      {/* </a> */}
+                    </React.Fragment>
+                  );
+                }}
+              </SidebarLinkGroup>
               {/* Hot Signal */}
               <SidebarLinkGroup activecondition={pathname.includes('')}>
                 {(handleClick, open) => {
@@ -237,7 +283,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                         }}
                       > */}
                         <div className="flex items-center justify-between">
-                        <Link to="/short">
+                        <Link to="/history">
                           <div className="flex items-center">
                             <svg className="shrink-0 h-10 w-10" viewBox="0 0 24 24">
                               <path
@@ -254,7 +300,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                               />
                             </svg>
                             <span className="text-lg font-medium ml-3">
-                              Short Signal
+                              History
                             </span>
                           </div>
                           </Link>
--- frontend/src/partials/dashboard/ShortSignal.jsx
+++ frontend/src/partials/dashboard/ShortSignal.jsx
@@ -51,9 +51,9 @@ function ShortSignal(short) {
                         <span dangerouslySetInnerHTML={{__html: default_btcIcon.replace('"32"', '"24"')}} />
                       }{item.symbol}
                     </li>
-                    <li className="text-center text-slate-300">{Number(item.price).toFixed(4)}</li>
+                    <li className="text-center text-slate-300">{Number(item.closePrice).toFixed(4)}</li>
                     <li className="text-center font-semibold text-red-500">{Number(item.change).toFixed(4)} %</li>
-                    <li className="text-center text-slate-300">{Number(item.hige).toFixed(4)}</li>
+                    <li className="text-center text-slate-300">{Number(item.high).toFixed(4)}</li>
                     <li className="text-center text-slate-300">{Number(item.low).toFixed(4)}</li>
                     <li className="text-center font-semibold text-red-500">short</li>
                   </ul>)
