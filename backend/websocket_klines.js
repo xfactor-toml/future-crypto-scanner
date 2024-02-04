ef1f107c5a73b80718ac3adb164c3b6902f5eb12
xfactor@rust <154992305+xfactor-toml@users.noreply.github.com>
2024-02-05 05:28:37 +0800

    Update websocket_klines.js

--- backend/websocket_klines.js
+++ backend/websocket_klines.js
@@ -20,7 +20,7 @@ exports.getRealTimeData =async (getData) => {
     const websocketThread = () => {
       //---------------------------------------------if data is confirm to test condition, push it.-----------------------------------------//
       const testData = async ( data ) => {
-        if( Math.abs( changePercent( data.openPrice, data.closePrice ) ) >= 0.5 ) {
+        if( Math.abs( changePercent( data.openPrice, data.closePrice ) ) >= 1.5 ) {
           const kline1d = await axios.get(`https://fapi.binance.com/fapi/v1/klines?symbol=${data.symbol}&interval=1d&limit=1`)      //    a day kline
           if( Number(kline1d?.data[0][7]) >= 1000000 ) {
             const kline1h = await axios.get(`https://fapi.binance.com/fapi/v1/klines?symbol=${data.symbol}&interval=1h&limit=1`);   //    a hour kline
